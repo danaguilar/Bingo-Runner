7a6e05dbdafbae369d239a6d8e9d19e5f03fbbdb
Daniel Aguilar <dan@Daniels-MBP.attlocal.net>
2017-07-19 16:00:51 -0500

    Added Bootstrap

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,28 @@
 class Game < ApplicationRecord
+	before_action :authenticate_user!
+
+	def index
+
+	end
+
+	def new 
+
+	end
+
+	def create
+
+	end
+
+	def update
+
+	end
+
+	def edit
+
+	end
+
+	def destroy
+
+	end
 	
 end
